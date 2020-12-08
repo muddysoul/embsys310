32dbc967e2ec42d55e8befc8fdb4a38d3dcbccc0
marisol <mbeck@hmc.edu>
2020-12-08 11:36:37 -0800

    image folder

--- assignment05/Swapping Pointers/main.c
+++ assignment05/Swapping Pointers/main.c
@@ -1,5 +1,25 @@
 
+void swap_pointer(int* x, int* y);
+
+
+void swap_pointer(int* x, int* y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+
 int main()
 {
+  int x = 20;
+  int y = 40;
+  int* xPtr = &x;
+  int* yPtr = &y;
+  
+  
+  //pause for debugger
+  swap_pointer(xPtr, yPtr);
+  
   return 0;
 }
